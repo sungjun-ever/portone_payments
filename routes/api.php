3480fe967834a3f92cf98b25a37b2ffd0455c3a5
임성준 <zkfmak10@gmail.com>
2025-06-30 21:23:37 +0900

    Edit route/api.php

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,17 @@ Route::prefix('v1')->group(function () {
         Route::get('/{id}', [MerchantController::class, 'show'])
             ->where('id', '[0-9]+');
         Route::put('/{id}', [MerchantController::class, 'update']);
+        Route::delete('/{id}', [MerchantController::class, 'destroy']);
 
         Route::get('/{id}/things', [MerchantThingController::class, 'index'])
             ->where('id', '[0-9]+');
         Route::post('/{id}/things', [MerchantThingController::class, 'store'])
             ->where('id', '[0-9]+');
-        Route::get('/{id}/things/{thingId}', [MerchantThingController::class, 'show'])
-            ->where('id', '[0-9]+')
+        Route::get('/things/{thingId}', [MerchantThingController::class, 'show'])
+            ->where('thingId', '[0-9]+');
+        Route::put('/things/{thingId}', [MerchantThingController::class, 'update'])
+            ->where('thingId', '[0-9]+');
+        Route::delete('/things/{thingId}', [MerchantThingController::class, 'destroy'])
             ->where('thingId', '[0-9]+');
     });
 })->withoutMiddleware('auth:sanctum');
