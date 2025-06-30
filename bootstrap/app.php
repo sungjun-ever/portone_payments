e58b5dd362be6f889718039188c23844e9ce24f9
임성준 <zkfmak10@gmail.com>
2025-06-30 21:20:05 +0900

    bootstap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -21,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->report(function (Throwable $exception): void {
+            Log::error($exception->getMessage(), request()->all());
+        });
+
         $exceptions->render(function (ValidationException $exceptions): JsonResponse {
             return response()->json([
                 'result' => 'error',
